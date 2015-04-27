2b44380afe3cf2206c880b3c161214e436ef10c1
SZanlongo <SZanlongo@users.noreply.github.com>
2015-04-27 13:08:28 -0400

    Updated singletestGrid and sampleexamples

--- irl_toolkit/General/sampleexamples.m
+++ irl_toolkit/General/sampleexamples.m
@@ -7,24 +7,44 @@ T = test_params.training_sample_lengths;
 example_samples = cell(N,T);
 
 % Sample trajectories.
-% fid = fopen('policy3.txt');
+fid(1) = fopen('t1.txt');
+fid(2) = fopen('t2.txt');
+fid(3) = fopen('t3.txt');
+fid(4) = fopen('t4.txt');
+fid(5) = fopen('t5.txt');
+fid(6) = fopen('t6.txt');
+fid(7) = fopen('t7.txt');
+fid(8) = fopen('t8.txt');
+fid(9) = fopen('t9.txt');
+fid(10) = fopen('t10.txt');
+s=zeros(1);
 for i=1:N,
     % Sample initial state.
-    % s = ceil(rand(1,1)*mdp_data.states);
-    s = 785;
-    
+     %s = ceil(rand(1,1)*mdp_data.states);
+    as = fgetl(fid(i));
+    %s=as;
+    s=as(1,1);
+    %size(s)
+    %s
     % Run sample trajectory.
     for t=1:T,
         % Compute optimal action for current state.
         a = feval(strcat(mdp_model,'action'),mdp_data,mdp_solution,s);
-        
+        %size(a)
         % Store example.
         example_samples{i,t} = [s;a];
         
         % Move on to next state.
+        % s = feval(strcat(mdp_model,'step'),mdp_data,mdp_solution,s,a);
+        if(t==1)
+            continue
+        else
+        s = fgetl(fid(i));
+        end
+        
+       % s=as(1,1);
         s = feval(strcat(mdp_model,'step'),mdp_data,mdp_solution,s,a);
-        %s = fgetl(fid);
-        %s = feval(strcat(mdp_model,'step'),mdp_data,mdp_solution,s,a);
         
     end;
+    i
 end;
--- irl_toolkit/singletestGrid.m
+++ irl_toolkit/singletestGrid.m
@@ -1,8 +1,8 @@
 % Convenience script for running a single test.
 addpaths;
-test_result = runtest('an',struct(),'linearmdp',...
+test_result = runtest(  'an', struct(),'linearmdp',...
     'gridworld',struct('n',50,'b',5,'determinism',0.8,'discount',0.9),...
-    struct('training_sample_lengths',127,'training_samples',1,'verbosity',2));
+    struct('training_sample_lengths',100,'training_samples',10,'verbosity',2));
  
 % Visualize solution.
 printresult(test_result);
